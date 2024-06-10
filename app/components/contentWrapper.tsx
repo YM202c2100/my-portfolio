053c54dff16f55b6c1988c2ef2cb56b096651005
YM202c2100 <y.m202c2100@gmail.com>
2024-06-10 11:09:13 +0900

    [feat] ボタンを押すことで内包する要素を縮小して表示できるように変更

--- app/components/contentWrapper.tsx
+++ app/components/contentWrapper.tsx
@@ -1,13 +1,17 @@
-import { ReactNode } from "react"
+"use client"
+
+import { ReactNode, useState } from "react"
 
 interface ContentWrapperProps{
   children:ReactNode
 }
 
 const ContentWrapper:React.FC<ContentWrapperProps> = ({children})=>{
+  const [menuOpen, setMenuOpen] = useState<boolean>()
   return(
-    <div>
+    <div className={`bg-green-300 transition-[transform] duration-500 ease-in-out ${menuOpen ? "scale-75":"scale-100"}`}>
       {children}
+      <button onClick={()=>setMenuOpen(!menuOpen)} className="border-2 px-3 py-2">メニュー</button>
     </div>
   )
 }
