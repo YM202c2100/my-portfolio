a43f01fc8ea36951e1a67e34239175145bbca6f9
YM202c2100 <y.m202c2100@gmail.com>
2024-06-10 16:16:51 +0900

    [feat] 画像を挿入しても問題なく縮小されることを確認

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 
 import ContentWrapper from "./components/contentWrapper";
+import Image from "next/image";
+
+import dragonImage from "@/public/polygonalRedDragon.png"
 
 export default function Page() {
   return (
     <ContentWrapper>
       <div>Home</div>
+      <Image src={dragonImage} alt="red polygonal dragon" unoptimized/>
     </ContentWrapper>
   );
 }
