73570e5d1e7beab15ae9ca8ad553c9f74ec4582a
YM202c2100 <y.m202c2100@gmail.com>
2024-06-10 16:36:22 +0900

    [feat] メニューボタンをビューポートに固定して表示するように変更

--- app/components/contentWrapper.tsx
+++ app/components/contentWrapper.tsx
@@ -12,7 +12,7 @@ const ContentWrapper:React.FC<ContentWrapperProps> = ({children})=>{
     <div className={`bg-green-300 transition-[transform] duration-500 ease-in-out ${menuOpen ? "scale-75":"scale-100"}`}>
       {children}
     </div>
-    <button onClick={()=>setMenuOpen(!menuOpen)} className="border-2 px-3 py-2">メニュー</button>
+    <button onClick={()=>setMenuOpen(!menuOpen)} className="bg-white border-2 px-3 py-2 fixed top-0 right-0">メニュー</button>
   </>)
 }
 
