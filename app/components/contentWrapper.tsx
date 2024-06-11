b57da250d7ffa4bf2d5ca8a512f6e13d8a428e8a
YM202c2100 <y.m202c2100@gmail.com>
2024-06-11 11:50:44 +0900

    [feat] ラッパー要素を基準とした、ビューポートのY軸中心の位置を取得できるように変更

--- app/components/contentWrapper.tsx
+++ app/components/contentWrapper.tsx
@@ -1,15 +1,29 @@
 "use client"
 
-import { ReactNode, useState } from "react"
+import { ReactNode, useEffect, useRef, useState } from "react"
 
 interface ContentWrapperProps{
   children:ReactNode
 }
 
 const ContentWrapper:React.FC<ContentWrapperProps> = ({children})=>{
+  const wrapperRef = useRef<HTMLDivElement>(null)
   const [menuOpen, setMenuOpen] = useState<boolean>()
+  const [originHeight, setOrigin] = useState<string>("0px")
+
+  useEffect(()=>{
+    if(wrapperRef.current){
+      const elemHeightCenter = wrapperRef.current.offsetHeight/2
+      const relativeYtoViewport = wrapperRef.current.getBoundingClientRect().y
+      setOrigin(`${elemHeightCenter-relativeYtoViewport}px`)
+    }
+  },[menuOpen])
+
   return(<>
-    <div className={`bg-green-300 transition-[transform] duration-500 ease-in-out ${menuOpen ? "scale-75":"scale-100"}`}>
+    <div 
+      className={`bg-green-300 transition-[transform] duration-500 ease-in-out ${menuOpen ? "scale-75":"scale-100"}`}
+      ref={wrapperRef}
+    >
       {children}
     </div>
     <button onClick={()=>setMenuOpen(!menuOpen)} className="bg-white border-2 px-3 py-2 fixed top-0 right-0">メニュー</button>
