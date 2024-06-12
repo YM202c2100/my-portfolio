2acf6cc883ebcabd23b46cdc8f6070ffa6512540
YM202c2100 <y.m202c2100@gmail.com>
2024-06-12 23:15:27 +0900

    [feat] メニュー表示方法をワープポータルを模した形式にするように変更したため、従来予定していた表示方法を削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-
-import ContentWrapper from "./components/contentWrapper";
 import Image from "next/image";
 
 import dragonImage from "@/public/polygonalRedDragon.png"
 
 export default function Page() {
-  return (
-    <ContentWrapper>
-      <div>Home</div>
-      <Image src={dragonImage} alt="red polygonal dragon" unoptimized/>
-    </ContentWrapper>
-  );
+  return (<>
+    <div>Home</div>
+    <Image src={dragonImage} alt="red polygonal dragon" unoptimized/>
+  </>);
 }
