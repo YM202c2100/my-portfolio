85d61477c07dbc75f465bb184e2e0602cfc6f04d
YM202c2100 <y.m202c2100@gmail.com>
2024-06-12 23:48:18 +0900

    [refactor] useEffectをWarpPortalのために使用したかったので、Menu内にWarpPortalを移動
    WarpPortal内でuseEffectを使用すると、メニュー切り替え時にその都度不必要なuseEffectが実行されるため

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,28 +1,31 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 interface LayoutProps{
   className?:string
 }
 
-const WarpPortal = ()=>{
-  return(
-    <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center r'>
-      <motion.div
-        className='w-96 h-96 bg-[#1c1a34] rounded-full blur-sm'
-        style={{boxShadow:
-          "-10px -10px 15px #db2a45, 0px -15px 20px #1e569b, 10px -10px 10px #1c1a34, 15px 0 20px #47ddf4, 10px 10px 20px #1c1a34, 0px 10px 10px #451e72, -11px 10px 20px #1c1a34"}}
-        animate={{rotate:360}}
-        transition={{repeat:Infinity, ease:"linear", duration:1.5}}
-      />
-    </div>
-  )
-}
+
 
 const Menu:React.FC = ()=>{
   const [isMenuOpen, setMenuOpen] = useState<boolean>(false)
+  useEffect(()=>{},[])
+
+  const WarpPortal = ()=>{
+    return(
+      <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center r'>
+        <motion.div
+          className='w-96 h-96 bg-[#1c1a34] rounded-full blur-sm'
+          style={{boxShadow:
+            "-10px -10px 15px #db2a45, 0px -15px 20px #1e569b, 10px -10px 10px #1c1a34, 15px 0 20px #47ddf4, 10px 10px 20px #1c1a34, 0px 10px 10px #451e72, -11px 10px 20px #1c1a34"}}
+          animate={{rotate:360}}
+          transition={{repeat:Infinity, ease:"linear", duration:1.5}}
+        />
+      </div>
+    )
+  }
 
   const MenuButton:React.FC<LayoutProps> = ({className})=>{
     return(
