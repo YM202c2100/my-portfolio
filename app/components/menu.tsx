d05ef868c6556cb8b250a44ed48ee17f04573408
YM202c2100 <y.m202c2100@gmail.com>
2024-06-13 00:11:57 +0900

    [feat] WarpPortalのサイズを画面から見切れない最大の大きさになるように変更

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -11,13 +11,16 @@ interface LayoutProps{
 
 const Menu:React.FC = ()=>{
   const [isMenuOpen, setMenuOpen] = useState<boolean>(false)
-  useEffect(()=>{},[])
+  const [isHeightShorterThanWidth, whichIsLonger] = useState<boolean>(false)
+  useEffect(()=>{
+    whichIsLonger(window.innerHeight < window.innerWidth)
+  },[])
 
   const WarpPortal = ()=>{
     return(
       <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center r'>
         <motion.div
-          className='w-96 h-96 bg-[#1c1a34] rounded-full blur-sm'
+          className={`${isHeightShorterThanWidth ? "h-5/6":"w-5/6"} aspect-square bg-[#1c1a34] rounded-full blur-sm`}
           style={{boxShadow:
             "-10px -10px 15px #db2a45, 0px -15px 20px #1e569b, 10px -10px 10px #1c1a34, 15px 0 20px #47ddf4, 10px 10px 20px #1c1a34, 0px 10px 10px #451e72, -11px 10px 20px #1c1a34"}}
           animate={{rotate:360}}
