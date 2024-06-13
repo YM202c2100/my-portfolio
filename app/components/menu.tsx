d3ca55fd5d79b765be4a8f16d3cff0d6b7a97762
YM202c2100 <y.m202c2100@gmail.com>
2024-06-13 10:16:22 +0900

    [refactor] orientationメディア特性を使用して、ポータルの大きさを、画面の高さと幅どちらに合わせるのかを決定できるように変更

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,27 +1,21 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 interface LayoutProps{
   className?:string
 }
 
-
-
 const Menu:React.FC = ()=>{
   const [isMenuOpen, setMenuOpen] = useState<boolean>(false)
-  const [isHeightShorterThanWidth, whichIsLonger] = useState<boolean>(false)
-  useEffect(()=>{
-    whichIsLonger(window.innerHeight < window.innerWidth)
-  },[])
 
   const WarpPortal = ()=>{
     return(
       <div className='fixed z-10 w-full h-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center'>
 
         <motion.div
-          className={`${isHeightShorterThanWidth ? "h-5/6":"w-5/6"} aspect-square bg-[#1c1a34] rounded-full blur-sm`}
+          className="landscape:h-5/6 portrait:w-5/6 aspect-square bg-[#1c1a34] rounded-full blur-sm"
           style={{boxShadow:
             "-10px -10px 15px #db2a45, 0px -15px 20px #1e569b, 10px -10px 10px #1c1a34, 15px 0 20px #47ddf4, 10px 10px 20px #1c1a34, 0px 10px 10px #451e72, -11px 10px 20px #1c1a34"}}
           animate={{rotate:360}}
