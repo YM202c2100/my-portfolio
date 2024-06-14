a5a8ee17e69933354755c781103f2c2f634c32ab
YM202c2100 <y.m202c2100@gmail.com>
2024-06-14 17:42:26 +0900

    [feat] ポータルのアニメーションにopacityに対する変化を追加

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -14,11 +14,14 @@ const Menu:React.FC = ()=>{
 
   const WarpPortal = ()=>{
     const portalAppearanceAnimation ={
-      initial:{scale:0},
-      animate:{scale:1},
+      initial:{scale:0, opacity:0},
+      animate:{scale:1, opacity:1},
       exit:{
         scale:0,
-        transition:{duration:0.1}
+        opacity:0,
+        transition:{
+          duration:0.1
+        }
       }
     }
     return(
