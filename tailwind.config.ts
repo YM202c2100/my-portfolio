1488c2f97420426037dff7c8157866abd47875fd
YM202c2100 <y.m202c2100@gmail.com>
2024-06-15 12:07:31 +0900

    [chore] 親要素にblurなどを付与して、子要素に動きを付与する形式にしたため、両者を融合させていたアニメーションを削除

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,6 @@ const config: Config = {
       },
 
       animation: {
-        unstableAppearance: 'unstableAppearance 1s ease-in-out',
         appear: 'appear 1s ease-in alternate',
         shift: 'shift 1s linear 2 alternate',
         glitch1: "glitch1 1s 2 alternate",
@@ -27,18 +26,6 @@ const config: Config = {
         glitchChildren: "glitch1 1s 2 alternate, appear 1s ease-in"
       },
       keyframes: {
-        unstableAppearance: {
-          '0%':{transform:'skewX(0deg) skewY(0deg)', filter:'blur(4px)',opacity:'0.3'},
-          '40%, 44%, 58%,  65%, 69%, 73%, 80%': { transform: 'skewX(0deg) skewY(0deg)' },
-          '41%': { transform: 'skewX(10deg)' },
-          '42%': { transform: 'skewX(-10deg)' },
-          '59%': { transform: 'skewX(40deg) skewY(10deg)' },
-          '62%': { transform: 'skewX(40deg) skewY(10deg)' },
-          '63%': { transform: 'skewX(-20deg) skewY(-10deg)' },
-          '70%': { transform: 'skewX(-20deg) skewY(-20deg)' },
-          '71%': { transform: 'skewX(10deg) skewY(-10deg) ',opacity:'1' },
-          '100%': {transform: 'skewX(0deg) skewY(0deg)', filter:'blur(0)'}
-        },
         appear:{
           "0%":{filter: "blur(4px)", opacity:"0.3"},
           "100%":{filter:"blur(0)", opacity:"1"}
